a6ba0d799081b93016acb639532e244b7c1efaa7
mengxiaopeng <mengxiaopeng@cvte.com>
2021-01-05 16:34:06 +0800

    * 1.docker-compose exec mysql mysql -uroot -p123456    * 2.retry-max="2" retry-interval="1" 进行验证

--- src/main/scala/com/oozie/pipineTest/PipineTestApp.scala
+++ src/main/scala/com/oozie/pipineTest/PipineTestApp.scala
@@ -83,4 +83,42 @@ class PipineTestApp {
       println("step 2 end  use oozie etl hive table")
     }
   }
+
+
+
+  /**
+   * 1.docker-compose exec mysql mysql -uroot -p123456
+   * 2.retry-max="2" retry-interval="1" 进行验证
+   * 验证OozieSqoopRetry
+   * 在xml中增加参数  【retry-max="2" retry-interval="1"】
+   */
+  @Test
+  def validationOozieSqoopRetry()= {
+    println("step 1 starting use sqoop import data into hive from mysql")
+    //表信息
+    conf.setProperty("mysqlUrl", "jdbc:mysql://172.20.211.54:3306")
+    conf.setProperty("mysqlDb", "mydb")
+    conf.setProperty("mysqlTable", "orders")
+    conf.setProperty("n","root")
+    conf.setProperty("p","123456")
+    conf.setProperty("hiveDb", "seewo_cdm_dev")
+    conf.setProperty("hiveTable", "orders")
+    conf.setProperty("extra", "--verbose")
+    conf.setProperty("map", "1")
+    //workflow.xml的地址
+    conf.setProperty("oozie.wf.application.path", "hdfs://cdhtest/user/mengxp/mysqlToHive/")
+    //本地文件不支持
+    //conf.setProperty("oozie.wf.application.path", "file:///Users/mengxp/Desktop/code/OozieProject/doc/test")
+    //存入hive的哪张表
+    val client = new OozieClient(oozieUrl)
+    val oozieJobId = client.run(conf)
+    println(conf)
+    while (client.getJobInfo(oozieJobId).getStatus.equals(WorkflowJob.Status.RUNNING)) {
+      println("Workflow-sqoop-import job running ...")
+      Thread.sleep(10 * 1000)
+    }
+    println("Workflow-sqoop-import job completed ...")
+    println(client.getJobInfo(oozieJobId))
+    println(s"step 1 end use sqoop import data into hive from mysql status =${client.getJobInfo(oozieJobId).getStatus}")
+  }
 }
